5913cf2c87a875a114d3c6e8d79587d7a5dfcd0f
yemregumus <ygumus@myseneca.ca>
2024-07-30 12:02:24 -0400

    angular reactive forms demo done

---
+++ Week10/AngularReactiveForms/angular-reactive-forms-demo/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'angular-reactive-forms-demo',
+  webDir: 'www'
+};
+
+export default config;
---
+++ Week10/AngularReactiveForms/angular-reactive-forms-demo/src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  myForm: FormGroup;
+  constructor() {
+    this.myForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    });
+  }
+
+  submitForm() {
+    console.log(this.myForm.value);
+  }
+}
